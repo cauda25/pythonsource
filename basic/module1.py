2998d5d1fdc3f9340365479c11e14d7413ad8fdc
ju <roronoa246@gmail.com>
2025-01-03 10:50:02 +0900

    모듈 작성

--- basic/module1.py
+++ basic/module1.py
@@ -1,15 +1,36 @@
+# 패키지 : 모듈 모음
+
 # 모듈 : 함수, 변수, 클래스를 모아 놓은 파이썬 파일
 #        .py 로 작성된 파일은 모듈임
 
 # 파이썬에서 제공되는 기본 모듈 사용하기
 # 모듈 불러오기
 # import 모듈명
-import math
 
-print(dir(math))  # math 가 제공해주는 함수들
+# import mod1
 
-# math 모듈애서 제공하는 함수 호출
-print(math.ceil(3.14))
-print(math.sin(1))
-print(math.cos(1))
-print(math.floor(3.14))
+# print(mod1.add(5, 8))
+# print(mod1.sub(8, 3))
+
+# add 함수만 삽입
+# from mod1 import add
+# mod1 모튤안에 있는 모든 요소
+from mod1 import *
+
+
+print(add(13, 9))
+print(sub(13, 9))
+
+from mod2 import prt1, prt2
+
+prt1()
+prt2()
+
+import mod3
+
+print(mod3.add(17, 5))
+
+m = mod3.Math()
+print(m.solv(6))
+
+print(mod3.PI)
